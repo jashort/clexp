e532d77864fe0c9009d4c29c06d43188c3511817
Jason Short <jason@sheersky.com>
2014-01-13 22:36:51 -0800

    Fixed failing tests (failed because decimal value was being tested as a float)

--- tests/test_expense.py
+++ tests/test_expense.py
@@ -1,13 +1,12 @@
 from unittest import TestCase
 import datetime
-
 from Expense import Expense
 
 
 class TestExpense(TestCase):
     def testCreateExpense(self):
         e = Expense("$2.53", "Living Expenses", "Rent and utilities", "12/1/2013")
-        self.failUnless(e.amount == 2.53)
+        self.failUnless(float(e.amount) == 2.53)
         self.failUnless(e.category == "Living Expenses")
         self.failUnless(e.description == "Rent and utilities")
         self.failUnless(e.date == datetime.date(2013, 12, 1))
@@ -16,9 +15,9 @@ class TestExpense(TestCase):
         self.failUnless(e.year == 2013)
         self.failUnless(str(e) == "12/01/2013	Living Expenses	Rent and utilities	$2.53")
 
-    def testCreateExpenseFloat(self):
+    def testCreateExpenseDecimal(self):
         e = Expense(2.53, "Living Expenses", "Rent and utilities", "12/1/2013")
-        self.failUnless(e.amount == 2.53)
+        self.failUnless(float(e.amount) == 2.53)
 
     def testCreateExpenseWithoutDate(self):
         today = datetime.datetime.now()
