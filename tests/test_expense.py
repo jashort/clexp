96d00f298268cc7f4ef31bee18256728b3379f84
Jason Short <jason@sheersky.com>
2014-01-18 23:29:56 -0800

    Changed CamelCase test names to lowercase with underscores

--- tests/test_expense.py
+++ tests/test_expense.py
@@ -4,7 +4,7 @@ from Expense import Expense
 
 
 class TestExpense(TestCase):
-    def testCreateExpense(self):
+    def test_create_expense(self):
         e = Expense("$2.53", "Living Expenses", "Rent and utilities", "12/1/2013")
         self.assertEqual(float(e.amount), 2.53)
         self.assertEqual(e.category, "Living Expenses")
@@ -15,31 +15,31 @@ class TestExpense(TestCase):
         self.assertEqual(e.year, 2013)
         self.assertEqual(str(e), "12/01/2013	Living Expenses	Rent and utilities	$2.53")
 
-    def testCreateExpenseFromDecimal(self):
+    def test_createExpense_from_decimal(self):
         e = Expense(2.53, "Living Expenses", "Rent and utilities", "12/1/2013")
         self.assertEqual(float(e.amount), 2.53)
 
-    def testCreateExpenseWithoutDate(self):
+    def test_create_expense_without_date(self):
         today = datetime.datetime.now()
         e = Expense(2.53, "Living Expenses", "Rent and utilities")
         self.assertEqual(e.date, today.date())
 
-    def testCreateExpenseWithFormula(self):
+    def test_create_expense_with_formula(self):
         e = Expense('5+4', 'Test', 'Test')
         self.assertEqual(int(e.amount), 9)
 
-    def testCreateExpenseWithComplicatedFormula(self):
+    def test_create_expense_with_complicated_formula(self):
         e = Expense('5.50 - 2.13 + 27/(3*2)', 'Test', 'Test')
         self.assertEqual(float(e.amount), 7.87)
 
-    def testCreateExpenseWithDivisionFormula(self):
+    def test_create_expense_with_division_formula(self):
         e = Expense('44/3', 'Test', 'Test')
         self.assertEqual(float(e.amount), 14.67)
 
-    def testCreateExpenseWithCharactersInFormula(self):
+    def test_create_expense_with_characters_in_formula(self):
         self.assertRaises(ValueError, Expense, '5*a', 'Test', 'Test')
 
-    def testCreateExpenseWithInvalidFormula(self):
+    def test_create_expense_with_invalid_formula(self):
         # Missing parenthesis in formula
         self.assertRaises(ValueError, Expense, '(3-2', 'Test', 'Test')
 
